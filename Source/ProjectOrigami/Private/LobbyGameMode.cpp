64349b2295d2e26fc2abc048f7c097a4150a49b6
Zihan Yang <zihan_yang_1996@outlook.com>
2025-03-21 17:58:00 -0400

    Add custom game mode for lobby.

---
+++ Source/ProjectOrigami/Private/LobbyGameMode.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyGameMode.h"
+#include "LobbyGameState.h"
+#include "LobbyPlayerState.h"
+
+ALobbyGameMode::ALobbyGameMode()
+{
+	// Set the default pawn class to the player pawn
+	GameStateClass = ALobbyGameState::StaticClass();
+	PlayerStateClass = ALobbyPlayerState::StaticClass();
+}
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	int32 NumPlayers{GameState->PlayerArray.Num()};
+	UE_LOG(LogTemp, Log, TEXT("Player Joined! Current Players: %d"), NumPlayers);
+
+	// Notify lobby UI, update player list, etc.
+}
+
+void ALobbyGameMode::Logout(AController* Exiting)
+{
+	Super::Logout(Exiting);
+
+	int32 NumPlayers{GameState->PlayerArray.Num()};
+	UE_LOG(LogTemp, Log, TEXT("Player Left! Current Players: %d"), NumPlayers);
+	// Notify lobby UI, update player list, etc.
+}
+
+void ALobbyGameMode::StartGame()
+{
+	if (HasAuthority())
+	{
+		UE_LOG(LogTemp, Log, TEXT("Starting game..."));
+		GetWorld()->ServerTravel("/Game/Maps/DefaultMap?listen");
+	}
+}
