c8212f36104e806aa7d5ddfcafb19f6047e4fdd0
Zihan Yang <zihan_yang_1996@outlook.com>
2025-03-21 17:56:14 -0400

    Add custom Game State for lobby.

---
+++ Source/ProjectOrigami/Public/LobbyGameState.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "LobbyGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTORIGAMI_API ALobbyGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+
+public:
+	// The PlayerArray is an array of all PlayerStates, auyomatically maintained on both server and clients
+	UFUNCTION(BlueprintCallable)
+	const TArray<APlayerState*>& GetPlayers() const { return PlayerArray; }
+};
