d342ead731851cc349ade5baee6597e6b9d8ba8c
Zihan Yang <zihan_yang_1996@outlook.com>
2025-03-22 19:50:51 -0400

    Update the Lobby Player State, to handle the ready state.

--- Source/ProjectOrigami/Private/LobbyPlayerState.cpp
+++ Source/ProjectOrigami/Private/LobbyPlayerState.cpp
@@ -10,13 +10,20 @@ void ALobbyPlayerState::SetReadyState(bool bNewIsReady)
 	if (HasAuthority())
 	{
 		bIsReady = bNewIsReady;
-		OnRep_bIsReady();  // RepNotify function needs to be called manually on the server
+		OnRep_bIsReady(); // RepNotify function needs to be called manually on the server
 	}
 }
 
+void ALobbyPlayerState::ServerSetReadyState_Implementation(bool bNewIsReady)
+{
+	SetReadyState(bNewIsReady);
+}
+
+
 void ALobbyPlayerState::OnRep_bIsReady()
 {
-	
+	// This will
+	UE_LOG(LogTemp, Log, TEXT("%s is now %s!"), *GetPlayerName(), bIsReady ? TEXT("Ready") : TEXT("Not Ready"));
 }
 
 void ALobbyPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
--- Source/ProjectOrigami/Public/LobbyPlayerState.h
+++ Source/ProjectOrigami/Public/LobbyPlayerState.h
@@ -15,13 +15,22 @@ class PROJECTORIGAMI_API ALobbyPlayerState : public APlayerState
 	GENERATED_BODY()
 
 public:
+	// Ready state that gets replicated to all clients
 	UPROPERTY(ReplicatedUsing=OnRep_bIsReady)
 	bool bIsReady{false};
 
+	// Server-side function to set the ready state
 	UFUNCTION(BlueprintCallable)
 	void SetReadyState(bool bNewIsReady);
+
+	// Clinent calls this function to request the server to set the ready state
+	// Marked reliable to ensure the server gets the message
+	UFUNCTION(Server, Reliable)
+	void ServerSetReadyState(bool bNewIsReady);
 
 protected:
 	UFUNCTION()
 	void OnRep_bIsReady();
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 };
